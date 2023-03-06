018294d84a9616e32796f0247f533995a2dd2dda
Caleb Lockwood <caleblockwood@iMac.local>
2023-03-05 17:20:57 -0700

    began routes in server.js

--- server.js
+++ server.js
@@ -8,3 +8,12 @@ const PORT = process.env.PORT || 3000;
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Set up routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
+  
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+  });
