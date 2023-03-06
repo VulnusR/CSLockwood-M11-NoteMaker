f9003d8cff84544c3e841cfdba0dd4d46077cc6a
Caleb Lockwood <caleblockwood@iMac.local>
2023-03-05 17:24:58 -0700

    routes continued + format changes 4 best practices in Server.js

--- server.js
+++ server.js
@@ -12,8 +12,18 @@ app.use(express.urlencoded({ extended: true }));
 // Set up routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  });
+});
   
-  app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
-  });
+app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    // Reads the db.json file and return the contents
+    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db', 'db.json')));
+    res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+});
