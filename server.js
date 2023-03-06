690c11b3ad058bdce8d01bc06a05f425371ba42f
Caleb Lockwood <caleblockwood@iMac.local>
2023-03-05 17:19:49 -0700

    added middleware to server.js

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
